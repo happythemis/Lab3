8cbbde7c7b50c26e86464def9c7d505693a5a3d1
happythemis <themislee1924@gmail.com>
2018-09-18 16:24:35 -0500

    second project commit

--- src/lab3anotherclass.java
+++ src/lab3anotherclass.java
@@ -1,6 +1,39 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class lab3anotherclass {
     public static void main(String[] unused) {
         // write your code here
-        System.out.println("Hee");
+        //System.out.println("Hee");
+        urlToString("http://erdani.com/tdpl/hamlet.txt");
+        String[] array = urlToString("http://erdani.com/tdpl/hamlet.txt").split("\\s+");
+        System.out.println(array.length);
+        int countspecific = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].toUpperCase().contains("PRINCE")) {
+                countspecific++;
+            }
+        }
+        System.out.println(countspecific);
+        //System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
+    }
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
     }
 }
